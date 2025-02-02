05fdfedb60fd28bca50a9a4df15a4515e43b9aee
Lugonul <mesakinmaksim@gmail.com>
2025-02-02 16:41:40 +0300

    Add files via upload

--- MainV2.java
+++ MainV2.java
@@ -1,42 +1,46 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class Main {
-    public static String culc(String input) throws Exception {
-        int a;
-        int b;
-        String operation;
-        int result;
+    public class MainV2 {
+        public static String culc(String input) throws Exception {
+            int a;
+            int b;
+            String operation;
+            int result;
 
-        String[] elements = input.replaceAll(" ","").split("(?<=[+\\-*/])|(?=[+\\-*/])");
-        if (elements.length != 3) throw new Exception("Недопустимый формат записи");
-        a = Integer.parseInt(elements[0]);
-        operation = elements[1];
-        b = Integer.parseInt(elements[2]);
-        if (a < 1 || a > 10 || b < 1 || b > 10) throw new Exception("Недопустимое число");
+            String[] elements = input.replaceAll(" ","").split("(?<=[+\\-*/])|(?=[+\\-*/])");
+            if (elements.length != 3) throw new Exception("Недопустимый формат записи");
+            try {
+                a = Integer.parseInt(elements[0]);
+                operation = elements[1];
+                b = Integer.parseInt(elements[2]);
+            }
+            catch (NumberFormatException e){
+                throw new NumberFormatException("Операции возможны только между целыми числами");
+            }
+            if (a < 1 || a > 10 || b < 1 || b > 10) throw new Exception("Недопустимое число");
 
-        switch (operation) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            case "/":
-                result = a / b;
-                break;
-            case "*":
-                result = a * b;
-                break;
-            default:
-                result = 0; // недостижимая строка, но без нее возникает ошибка того, что переменной result может быть не присвоенно значение.
+            switch (operation) {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                default:
+                    result = 0; // недостижимая строка, но без нее возникает ошибка того, что переменной result может быть не присвоенно значение.
+            }
+            return String.valueOf(result);
         }
-        return String.valueOf(result);
-    }
 
-    public static void main(String[] args) throws Exception {
-        System.out.print("input: ");
-        Scanner scan = new Scanner(System.in);
-        System.out.println("output: " + culc(scan.nextLine()));
+        public static void main(String[] args) throws Exception {
+            System.out.print("input: ");
+            Scanner scan = new Scanner(System.in);
+            System.out.print("output: " + culc(scan.nextLine()));
+        }
     }
-}
-
